3683d107bb68f1d0574de04b22e627f0185ca552
Justin Synnott <119279210+justsynnott2@users.noreply.github.com>
2026-01-13 17:33:54 -0500

    Add files via upload

---
+++ Chat-App-gRPC/app/src/main/java/edu/stevens/cs522/chat/databases/ChatDatabase.java
@@ -0,0 +1,47 @@
+package edu.stevens.cs522.chat.databases;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import edu.stevens.cs522.chat.entities.Chatroom;
+import edu.stevens.cs522.chat.entities.Counter;
+import edu.stevens.cs522.chat.entities.Message;
+import edu.stevens.cs522.chat.entities.Peer;
+import edu.stevens.cs522.chat.entities.TimestampConverter;
+import edu.stevens.cs522.chat.entities.UUIDConverter;
+
+/**
+ * Created by dduggan.
+ *
+ * See build.gradle file for app for where schema file is left after processing.
+ */
+
+@Database(entities = {Peer.class, Message.class, Counter.class, Chatroom.class}, version = 2, exportSchema = true)
+@TypeConverters({TimestampConverter.class, UUIDConverter.class})
+// TODO Add annotations (including @TypeConverters)
+public abstract class ChatDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "messages.db";
+
+    private static ChatDatabase instance;
+
+    public abstract PeerDao peerDao();
+
+    public abstract ChatroomDao chatroomDao();
+
+    public abstract MessageDao messageDao();
+
+    public abstract RequestDao requestDao();
+
+    public static ChatDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, ChatDatabase.class, DATABASE_NAME).build();
+        }
+        return instance;
+    }
+
+}
