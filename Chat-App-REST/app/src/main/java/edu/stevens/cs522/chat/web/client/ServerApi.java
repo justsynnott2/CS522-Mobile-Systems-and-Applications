db67648b2701154a664c2532e90f0f6b033d9b5f
Justin Synnott <119279210+justsynnott2@users.noreply.github.com>
2026-01-13 17:34:45 -0500

    Add files via upload

---
+++ Chat-App-REST/app/src/main/java/edu/stevens/cs522/chat/web/client/ServerApi.java
@@ -0,0 +1,25 @@
+package edu.stevens.cs522.chat.web.client;
+
+import edu.stevens.cs522.chat.entities.Message;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/*
+ * The API for the chat server.
+ *
+ * TODO annotate the methods with HTTP operations and context paths
+ */
+public interface ServerApi {
+
+    public final static String CHAT_NAME = "chat-name";
+
+    @POST("/chat")
+    public Call<Void> register(@Query(CHAT_NAME) String chatName);
+
+    @POST("/chat/{chat-name}")
+    public Call<Void> postMessage(@Path(CHAT_NAME) String chatName, @Body Message chatMessage);
+
+}
